ee6093fac234fa37bec6bc964c10b35bb82ea9e7
Nayeem Reza <reza.uranium@gmail.com>
2024-12-14 17:40:42 +0100

    2024:14:2

--- 2024/14/solution.js
+++ 2024/14/solution.js
@@ -22,12 +22,42 @@ function processLines(inputLines) {
 function getFinalLocation(item, r, c, time) {
   let x = (item.p.x + time * item.v.x) % r;
   let y = (item.p.y + time * item.v.y) % c;
-
   // handle negative values
   x = x < 0 ? x + r : x;
   y = y < 0 ? y + c : y;
 
-  return [x, y];
+  return [Math.abs(x), Math.abs(y)];
+}
+
+function findTheTree(listOfItems, r, c) {
+  let treeAtTime = null;
+  for (let time = 1; time <= r * c; time++) {
+    const map = new Map();
+    for (item of listOfItems) {
+      const [x, y] = getFinalLocation(item, r, c, 1);
+      item.p.x = x;
+      item.p.y = y;
+      map.set(`${x},${y}`, (map.get(`${x},${y}`) || 0) + 1);
+    }
+
+    // if all the map entries have only 1 count, then print the time
+    if (Array.from(map.values()).every(v => v === 1)) {
+      treeAtTime = time;
+
+      // iterate through r*c and if map[i][j] exists then print # else print .
+      for (let i = 0; i < r; i++) {
+        let row = "";
+        for (let j = 0; j < c; j++) {
+          row += map.get(`${i},${j}`) ? "#" : ".";
+        }
+        console.log(row);
+      }
+
+      break;
+    }
+  }
+
+  return treeAtTime;
 }
 
 function calculateSafetyFactor(listOfItems) {
@@ -37,8 +67,6 @@ function calculateSafetyFactor(listOfItems) {
     const [x, y] = getFinalLocation(item, r, c, 100);
     map.set(`${x},${y}`, (map.get(`${x},${y}`) || 0) + 1);
   }
-
-  console.log(map);
 
   // calculate the individual total count of each quadrant of 11 x 7 grid
   // and then multiple 4 of them to get the safety factor
@@ -52,14 +80,11 @@ function calculateSafetyFactor(listOfItems) {
 
   for (const [x1, x2, y1, y2] of quadrants) {
     let count = 0;
-    console.log(x1, x2, y1, y2);
     for (let x = x1; x < x2; x++) {
       for (let y = y1; y < y2; y++) {
-        console.log(`${x},${y}`, map.get(`${x},${y}`));
         count += map.get(`${x},${y}`) || 0;
       }
     }
-    console.log(count);
     safetyFactor *= count;
   }
 
@@ -67,10 +92,15 @@ function calculateSafetyFactor(listOfItems) {
 }
 
 function main() {
+  const r = 103, c = 101;
   const inputLines = readInputFile(inputFilePath);
   const listOfItems = processLines(inputLines);
 
-  console.log(calculateSafetyFactor(listOfItems));
+  const safetyFactor = calculateSafetyFactor(listOfItems, r, c);
+  console.log("PART 1: ", safetyFactor);
+
+  const treeAtTime = findTheTree(listOfItems, r, c);
+  console.log("PART 2: ", treeAtTime);
 }
 
 main();
