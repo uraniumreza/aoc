1cd69858f8e8fc2b8f1ecd6015b926dfe2f2e606
Md Nayeem Reza <nayeem.reza@embl.de>
2024-12-15 23:23:33 +0100

    2024:15:2

--- 2024/15/solution.js
+++ 2024/15/solution.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const { readInputFile } = require('../lib/read-file');
 
-const inputFilePath = path.join(__dirname, 'input1.txt');
+const inputFilePath = path.join(__dirname, 'input2.txt');
 
 function processLines(inputLines) {
   const map = [];
@@ -20,60 +20,93 @@ function processLines(inputLines) {
     }
   });
 
-  let x = 0;
-  let y = 0;
-  for (let i = 0; i < map.length; i++) {
-    for (let j = 0; j < map[i].length; j++) {
-      if (map[i][j] === "@") {
-        x = i;
-        y = j;
-        break;
-      }
-    }
-  }
-
-  return [map, moves.flat(), x, y];
+  return [map, moves.flat()];
 }
 
-function isMoveAllowed(map, move, x, y) {
-  let isAllowed = true;
-  let newX = x;
-  let newY = y;
-  if (move == '<') {
-    while (map[newX][newY] !== ".") {
-      if (map[newX][newY] === "#") {
-        isAllowed = false;
+function findBoxesToBeMoved(map, move, x, y) {
+  const connectedBoxes = [];
+  const stack = [[x, y]];
+  const visited = new Set();
+
+  if (move == "<") {
+    while (stack.length > 0) {
+      let [_x, _y] = stack.pop();
+      visited.add(`${_x},${_y}`);
+
+      if (map[_x][_y - 1] === '.') {
+        if (stack.length === 0) break;
+        else continue;
+      } else if (map[_x][_y - 1] === '#') {
+        visited.clear();
         break;
+      } else {
+        stack.push([_x, _y - 1]);
       }
-      newY--;
     }
-  } else if (move == '>') {
-    while (map[newX][newY] !== ".") {
-      if (map[newX][newY] === "#") {
-        isAllowed = false;
+  } else if (move == ">") {
+    while (stack.length > 0) {
+      let [_x, _y] = stack.pop();
+      visited.add(`${_x},${_y}`);
+
+      if (map[_x][_y + 1] === '.') {
+        if (stack.length === 0) break;
+        else continue;
+      } else if (map[_x][_y + 1] === '#') {
+        visited.clear();
         break;
+      } else {
+        stack.push([_x, _y + 1]);
       }
-      newY++;
     }
-  } else if (move == '^') {
-    while (map[newX][newY] !== ".") {
-      if (map[newX][newY] === "#") {
-        isAllowed = false;
+  } else if (move == "^") {
+    while (stack.length > 0) {
+      let [_x, _y] = stack.pop();
+      visited.add(`${_x},${_y}`);
+
+      if (map[_x - 1][_y] === '.') {
+        if (stack.length === 0) break;
+        else continue;
+      } else if (map[_x - 1][_y] === '#') {
+        visited.clear();
         break;
+      } else {
+        stack.push([_x - 1, _y]);
+        if (map[_x - 1][_y] === ']') {
+          stack.push([_x - 1, _y - 1]);
+        } else if (map[_x - 1][_y] === '[') {
+          stack.push([_x - 1, _y + 1]);
+        }
       }
-      newX--;
+      // console.log(stack);
     }
-  } else if (move == 'v') {
-    while (map[newX][newY] !== ".") {
-      if (map[newX][newY] === "#") {
-        isAllowed = false;
+  } else if (move == "v") {
+    while (stack.length > 0) {
+      let [_x, _y] = stack.pop();
+      visited.add(`${_x},${_y}`);
+
+      if (map[_x + 1][_y] === '.') {
+        if (stack.length === 0) break;
+        else continue;
+      } else if (map[_x + 1][_y] === '#') {
+        visited.clear();
         break;
+      } else {
+        stack.push([_x + 1, _y]);
+        if (map[_x + 1][_y] === ']') {
+          stack.push([_x + 1, _y - 1]);
+        } else if (map[_x + 1][_y] === '[') {
+          stack.push([_x + 1, _y + 1]);
+        }
       }
-      newX++;
     }
   }
 
-  return [isAllowed, newX, newY];
+  for (const entry of visited) {
+    const [cx, cy] = entry.split(",").map(Number);
+    connectedBoxes.push([cx, cy]);
+  }
+
+  return connectedBoxes;
 }
 
 function moveRobot(move, x, y, newX, newY, map) {
@@ -91,13 +124,25 @@ function moveRobot(move, x, y, newX, newY, map) {
     }
     _y = y + 1;
   } else if (move == '^') {
-    for (let i = newX; i < x; i++) {
-      map[i][y] = map[i + 1][y];
+    const visited = new Set();
+    const connectedBoxes = dfs(map, x, y, move, visited);
+
+    for (const [cx, cy] of connectedBoxes) {
+      for (let i = cx; i > newX; i--) {
+        map[i][cy] = map[i - 1][cy];
+      }
+      map[newX][cy] = '.';
     }
     _x = x - 1;
   } else if (move == 'v') {
-    for (let i = newX; i > x; i--) {
-      map[i][y] = map[i - 1][y];
+    const visited = new Set();
+    const connectedBoxes = dfs(map, x, y, move, visited);
+
+    for (const [cx, cy] of connectedBoxes) {
+      for (let i = cx; i < newX; i++) {
+        map[i][cy] = map[i + 1][cy];
+      }
+      map[newX][cy] = '.';
     }
     _x = x + 1;
   }
@@ -106,20 +151,77 @@ function moveRobot(move, x, y, newX, newY, map) {
   return [_x, _y];
 }
 
-function getSumOfGpsCoordinates(map, moves, x, y) {
-  for (move of moves) {
-    const [isAllowed, newX, newY] = isMoveAllowed(map, move, x, y);
-    // console.log(move, isAllowed, x, y, newX, newY);
-    if (isAllowed) {
-      [x, y] = moveRobot(move, x, y, newX, newY, map);
+function moveBoxes(direction, boxes, map) {
+  let newX, newY;
+
+  if (direction == "<") {
+    // sort boxes so that we can start moving from the leftmost box
+    boxes.sort((a, b) => a[1] - b[1]);
+    newX = boxes.at(-1)[0];
+    newY = boxes.at(-1)[1] - 1;
+
+    for (const box of boxes) {
+      const [x, y] = box;
+      map[x][y - 1] = map[x][y];
     }
-    // console.log(map);
+  } else if (direction == ">") {
+    // sort boxes so that we can start moving from the rightmost box
+    boxes.sort((a, b) => b[1] - a[1]);
+    newX = boxes.at(-1)[0];
+    newY = boxes.at(-1)[1] + 1;
+
+    for (const box of boxes) {
+      const [x, y] = box;
+      map[x][y + 1] = map[x][y];
+    }
+  } else if (direction == "^") {
+    // sort boxes so that we can start moving from the topmost box
+    boxes.sort((a, b) => a[0] - b[0]);
+    newX = boxes.at(-1)[0] - 1;
+    newY = boxes.at(-1)[1];
+
+    for (const box of boxes) {
+      const [x, y] = box;
+      map[x - 1][y] = map[x][y];
+      map[x][y] = ".";
+    }
+  } else if (direction == "v") {
+    // sort boxes so that we can start moving from the bottommost box
+    boxes.sort((a, b) => b[0] - a[0]);
+    newX = boxes.at(-1)[0] + 1;
+    newY = boxes.at(-1)[1];
+
+    for (const box of boxes) {
+      const [x, y] = box;
+      map[x + 1][y] = map[x][y];
+      map[x][y] = ".";
+    }
   }
+
+  return [newX, newY];
+}
+
+function getSumOfGpsCoordinates(map, moves, x, y) {
+  let idx = 0;
+  for (move of moves) {
+    const boxes = findBoxesToBeMoved(map, move, x, y);
+    // console.log(`============== Move: ${idx++}`, move);
+
+    if (boxes.length > 0) {
+      [newX, newY] = moveBoxes(move, boxes, map);
+      map[x][y] = ".";
+      x = newX;
+      y = newY;
+    }
+    // mapString = map.map(row => row.join("")).join("\n");
+    // console.log(mapString);
+  }
+
 
   let sum = 0;
   for (let i = 0; i < map.length; i++) {
     for (let j = 0; j < map[i].length; j++) {
-      if (map[i][j] === "O") {
+      if (map[i][j] === "[") {
         const coordinate = 100 * i + j;
         sum += coordinate;
       }
@@ -129,13 +231,51 @@ function getSumOfGpsCoordinates(map, moves, x, y) {
   return sum;
 }
 
+function trasformMap(map) {
+  let newMap = [];
+  for (let i = 0; i < map.length; i++) {
+    newMap.push([]);
+    for (let j = 0; j < map[i].length; j++) {
+      if (map[i][j] === "#") {
+        newMap[i].push("#", "#");
+      } else if (map[i][j] === ".") {
+        newMap[i].push(".", ".");
+      } else if (map[i][j] === "O") {
+        newMap[i].push("[","]");
+      } else if(map[i][j] === "@") {
+        newMap[i].push("@",".");
+      }
+    }
+  }
+
+  return newMap;
+}
+
+function findRobotPosition(map) {
+  let x = 0;
+  let y = 0;
+  for (let i = 0; i < map.length; i++) {
+    for (let j = 0; j < map[i].length; j++) {
+      if (map[i][j] === "@") {
+        x = i;
+        y = j;
+        break;
+      }
+    }
+  }
+
+  return [x, y];
+}
+
 function main() {
   const inputLines = readInputFile(inputFilePath);
-  const [map, moves, x, y] = processLines(inputLines);
+  const [map, moves] = processLines(inputLines);
 
-  // console.log(map, moves);
-  const sumOfGpsCoordinates = getSumOfGpsCoordinates(map, moves, x, y);
-  console.log("PART 1: ", sumOfGpsCoordinates);
+  const newMap = trasformMap(map);
+  const [x,y] = findRobotPosition(newMap);
+  // console.log(newMap.map(row => row.join("")).join("\n"));
+  const sumOfGpsCoordinates = getSumOfGpsCoordinates(newMap, moves, x, y);
+  console.log("PART 2: ", sumOfGpsCoordinates);
 }
 
 main();
